10e1fee4282308065f76a2b02a81754c35d1c6ce
Boglarka1212 <kazmer.boglarka@gmail.com>
2026-03-03 13:45:57 +0100

    oprettede const animals i js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 "use strict"
 
+const animals = [
+    {name: "Simba", species: "Løve", age: 5, food: "Kød"},
+    {name: "Dumbo", species: "Elefant", age: 8, food: "Blade og frugt"},
+    {name: "Gerald", species: "Giraf", age: 6, food: "Blade fra høje træer"}
+];
